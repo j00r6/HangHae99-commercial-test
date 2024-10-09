5517bf20fab796ce99b8aaffb934068d270e47d1
j00r6 <jindory8@gmail.com>
2024-10-09 13:36:13 +0900

    docker setting complete

---
+++ src/main/java/com/commercialtest/Hanghae99CommercialTestApplication.java
@@ -0,0 +1,13 @@
+package com.commercialtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghae99CommercialTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanghae99CommercialTestApplication.class, args);
+	}
+
+}
